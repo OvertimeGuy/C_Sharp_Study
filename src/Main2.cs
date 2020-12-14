a0e5f4a8c7632fa7860775e4851ef64f3c5c96d2
‍신문영[학생](소프트웨어융합대학 소프트웨어융합학과) <creator0530@khu.ac.kr>
2020-12-14 14:29:46 +0000

    2020.12.14 최종 테스트

---
+++ src/Main2.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class main3 
+{
+	public static void Main() {
+		Console.WriteLine("Hello, goorm!");
+	}
+}
+public class main2 
+{
+	public static void Main22() {
+		Console.WriteLine("hihi22");
+	}
+}
